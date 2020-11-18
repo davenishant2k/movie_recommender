fc4d4bfb50403e4b0ffc4629d466c57672ebb16e
Onkar Bagwe <onkarbagwe25@gmail.com>
2020-11-19 00:40:13 +0530

    final changes

--- movie_app/models.py
+++ movie_app/models.py
@@ -16,7 +16,7 @@ GENRE_CHOICES = (
 )
 
 class Movie(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length = 100, unique=True)
     description = models.TextField(max_length = 1000)
     image = models.ImageField(upload_to = 'movie')
     cast = models.CharField(max_length = 500)
